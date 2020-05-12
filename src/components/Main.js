daf315c079fcb41814bf1ea34fda213b6fc5c005
powsokan <powsi1997@gmail.com>
2020-05-12 16:44:12 +0530

    api calling method

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,19 @@
 import React from "react";
+import axios from "axios";
 
-const  Main =()=>{
-    return(
+import Header from "./Header";
+
+const API_KEY = "66aa94f39e438aad5573cbf1112de56f"
+
+
+const Main = () => {
+    const api_call = () => {
+        const url = `api.openweathermap.org/data/2.5/weather?q=${city name}&appid=${API_KEY}`
+        axios.get(url)
+    }
+    return (
         <div className="main">
+            <Header/>
 
         </div>
     )
