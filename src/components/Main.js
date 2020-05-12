d2ce616b43dd525b6da200346ecdd271c024ffcf
powsokan <powsi1997@gmail.com>
2020-05-12 17:16:08 +0530

    components added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
 import Header from "./Header";
@@ -8,22 +8,23 @@ import WeatherSearch from "./WeatherSearch";
 
 
 const Main = () => {
-    const api_call = async () => {
+    const [weather,setWeather]=useState()
+    const api_call = async e => {
+        e.preventDefault()
         const API_KEY = "66aa94f39e438aad5573cbf1112de56f"
         const url = `https://api.openweathermap.org/data/2.5/weather?q=Liverpool&appid=${API_KEY}`
         const request = axios.get(url)
         const response = await request
+        setWeather(response.data.main)
     }
 
-    useEffect(() => {
-        api_call()
-    }, [])
+
 
     return (
         <div className="main">
             <Header/>
             <Content>
-                <WeatherSearch/>
+                <WeatherSearch api_call={api_call}/>
             </Content>
 
         </div>
--- src/components/WeatherSearch.js
+++ src/components/WeatherSearch.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const WeatherSearch = () => {
+const WeatherSearch = (props) => {
     return (
         <div className="weather-search">
-            <form className="weather-search__form">
+            <form onSubmit={props.api_call} className="weather-search__form">
                 <input type="text" className="weather-search__input" autoComplete="off"/>
                 <div className="weather-search__submit">
                     <button className="weather-search__button">&rarr;</button>
