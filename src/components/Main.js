5e41990a5818e415692e8be4f181c30d2c12f590
powsokan <powsi1997@gmail.com>
2020-05-12 17:02:42 +0530

    components added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import axios from "axios";
 
 import Header from "./Header";
+import Content from "./Content";
+import WeatherSearch from "./WeatherSearch";
 
-const API_KEY = "66aa94f39e438aad5573cbf1112de56f"
 
 
 const Main = () => {
-    const api_call = () => {
-        const url = `api.openweathermap.org/data/2.5/weather?q=${city name}&appid=${API_KEY}`
-        axios.get(url)
+    const api_call = async () => {
+        const API_KEY = "66aa94f39e438aad5573cbf1112de56f"
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=Liverpool&appid=${API_KEY}`
+        const request = axios.get(url)
+        const response = await request
     }
+
+    useEffect(() => {
+        api_call()
+    }, [])
+
     return (
         <div className="main">
             <Header/>
+            <Content>
+                <WeatherSearch/>
+            </Content>
 
         </div>
     )
---
+++ src/components/WeatherSearch.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const WeatherSearch = () => {
+    return (
+        <div className="weather-search">
+            <form className="weather-search__form">
+                <input type="text" className="weather-search__input" autoComplete="off"/>
+                <div className="weather-search__submit">
+                    <button className="weather-search__button">&rarr;</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+export default WeatherSearch
