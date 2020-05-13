16c15e983dd168ef64fdc1f9f94c2fd99d1e598e
powsokan <powsi1997@gmail.com>
2020-05-13 07:18:18 +0530

    Updates

---
+++ precache-manifest.8187640ee83543f6edbc33dc94521090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4064ce564f0574b9cb66349c6a71e5df",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "162e8149ee2d8d6ed4c1",
+    "url": "/WeatherApp/static/css/main.28971894.chunk.css"
+  },
+  {
+    "revision": "9d50ab26ff17d570dd96",
+    "url": "/WeatherApp/static/js/2.2619da21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.2619da21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162e8149ee2d8d6ed4c1",
+    "url": "/WeatherApp/static/js/main.b8370239.chunk.js"
+  },
+  {
+    "revision": "a1be24b28b4289363924",
+    "url": "/WeatherApp/static/js/runtime-main.07d82fdf.js"
+  }
+]);
